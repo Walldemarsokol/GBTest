773bd90467edc981f52717db6a8f9aa0bfc743a5
VladimirSokol <walldemarsokol@yandex.ru>
2022-09-30 16:50:21 +0300

    add second half test (method`s)

--- Test for GB/Program.cs
+++ Test for GB/Program.cs
@@ -12,4 +12,38 @@ string[] array = new string[length];
     array[i] = elem;
 }
 
+WriteLine("Начальный массив: ");
+Write("[");
+PrintArray(array);
+Write("]");
+WriteLine();
+WriteLine("Результат работы программы: ");
+WriteLine();
+PrintArray(ConvertArray(array,length));
+WriteLine();
 
+// ---------------------набор используемых методов----------------------------
+
+void PrintArray(string [] filledArray){
+    for(int i = 0; i<filledArray.Length;i++){
+        Write(filledArray[i]+" ");
+    }
+}
+
+
+string[] ConvertArray(string[] array,int size)
+{   
+string[] arrayFinal = new string[size];
+int length = 3;                                    
+int position = 0;
+
+for (int j = 0; j < size; j++)
+{
+    if (array[j].Length <= length)
+    {
+        arrayFinal[position] = array[j];
+        position++;
+    }
+}
+return arrayFinal;
+}
