6231d55e97bb4729ffd5b9d551c780b1ce2990d3
VladimirSokol <walldemarsokol@yandex.ru>
2022-09-30 16:48:48 +0300

    add half of test

--- Test for GB/Program.cs
+++ Test for GB/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;  
+
+Clear();
+WriteLine("Введите длинну массива: ");  
+int length = Convert.ToInt32(ReadLine());   //пользователь задает длину массива 
+string[] array = new string[length];
+
+    for (int i = 0; i < array.Length; i++)
+{
+    WriteLine($"Введите {i+1}-й элемент массива: ");            
+    string elem = Convert.ToString(ReadLine());         
+    array[i] = elem;
+}
+
+
